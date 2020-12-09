621d5c673592650a07f405555e2a9fe179e6a82c
ventdraco <ventdraco@gmail.com>
2020-12-08 18:02:53 -0800

    Plot 1 for project

---
+++ plot1.R
@@ -0,0 +1,23 @@
+library(dplyr)
+filename <- "Dataset.zip"
+##checking if the file already exists, if it doesn't it will download it
+if (!file.exists(filename)){
+        fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(fileURL, filename, method="curl")
+} 
+##unzip the file, if it doesn't exists on the path.
+if (!file.exists("Exploratory Analisys")) { 
+        unzip(filename) 
+}
+##checking files
+list.files("./", recursive=TRUE)
+
+##Reading data
+hpc<- read.table("household_power_consumption.txt", sep = ";" ,header = TRUE)
+xhpc <- subset(hpc,Date=="1/2/2007" | Date =="2/2/2007")
+
+#calling the hist function
+hist(as.numeric(as.character(xhpc$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+        
+# annotating graph
+title(main="Global Active Power")
